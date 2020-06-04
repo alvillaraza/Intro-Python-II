93bc0a3f20d929fd0704c15baac40d9916c98019
alexis villaraza <alexis.borja@gmail.com>
2020-06-03 21:25:55 -0700

    wrote while loop

--- src/adv.py
+++ src/adv.py
@@ -1,10 +1,25 @@
-from room import Room
+# class Room:
+#     def __init__(self, name, desc):
+#         self.name = name
+#         self.desc = desc
 
+
+# class Player:
+#     def __init__(self, name, room):
+#         self.name = name
+#         self.room = room
+
+from room import Room
+from player import Player
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
+    #  {
+    # 'name' :'Outside Cave Entrance',
+    # 'desc': 'North of you, the cave mount beckons'
+    # }
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
@@ -21,7 +36,6 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
@@ -37,15 +51,48 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
-
+# # Make a new player object that is currently in the 'outside' room.
+# currentPlayer = Player(input("what is your name?"), input("what room?"))
+me = Player('alexis', room['outside'])
 # Write a loop that:
-#
-# * Prints the current room name
+while True:
+    # * Prints the current room name
+    print(me.name, me.room.name, me.room.desc)
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
+    direction = input('Where would you like to go?')
+    print(direction)
 #
+
 # If the user enters a cardinal direction, attempt to move to the room there.
+
+    # me.room = me.room.{direction}_to
+    if direction == "n":
+        me.room = me.room.n_to
+        print(me.room.name)
+
+    elif direction == 's':
+        me.room = me.room.s_to
+        print(me.room.name)
+
+    elif direction == 'e':
+        me.room = me.room.e_to
+        print(me.room)
+
+    elif direction == 'w':
+        me.room = me.room.w_to
+        print(me.room.name)
+
+    # elif:
+    #     print("You're lost")
+    
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+    elif direction == 'q':
+        print('Game Over')
+        break
+    
+    else:
+        print('youre lost')
+        
